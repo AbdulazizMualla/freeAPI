fe7baa261280abd36c4c42f61f933c10cdb25c67
AbdulazizMualla <amgad74@hotmail.com>
2022-10-21 22:05:47 +0300

    change endpoint

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import {createStore} from "vuex";
-const api_url = 'http://127.0.0.1:8000/api/';
+const api_url = 'https://tawfig.info/api/';
 export const store = createStore({
     state(){
         return{
